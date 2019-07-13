01fe6160dbe40bd336397297387e2a0b45777b1a
WqyJh <781345688@qq.com>
2019-07-13 17:49:17 +0800

    feat: remove python 2 support

--- sshx/__init__.py
+++ sshx/__init__.py
@@ -1,6 +1,3 @@
-
-from __future__ import nested_scopes, generators, division, absolute_import, with_statement, print_function, unicode_literals
-
 __version__ = '0.3.1'
 
 __all__ = ['sshx']
--- sshx/const.py
+++ sshx/const.py
@@ -1,5 +1,3 @@
-from __future__ import nested_scopes, generators, division, absolute_import, with_statement, print_function, unicode_literals
-
 DEFAULT_USER = 'root'
 DEFAULT_HOST = None
 DEFAULT_PORT = '22'
