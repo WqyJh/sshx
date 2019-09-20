f837e6dd3f7b5ac88ac9f0c9905ee8baeaa793bd
WqyJh <781345688@qq.com>
2019-09-21 04:31:51 +0800

    chore: use pipenv to replace pip

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-dep="codecov mock twine"
+dep="pipenv"
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-
-    # Install some custom requirements on OS X
-    # e.g. brew install pyenv-virtualenv
-
-    case "${PY}" in
-        py27)
-            brew install python@2
-            brew link --overwrite python@2
-            pip install $dep
-            python setup.py install
-            ;;
-        py36)
-            brew upgrade python
-            pip3 install $dep
-            python3 setup.py install
-            ;;
-    esac
+    brew upgrade python
+    pip3 install $dep
+    pipenv install --dev --system
+    python3 setup.py install
 else
     pip install $dep
+    pipenv install --dev --system
     python setup.py install
 fi 
